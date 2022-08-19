e45f7f7db19ffe49cfbbf3a72b8fc233bd342adf
Ricardo Cheron <rcheron1@live.maryville.edu>
2022-08-19 09:18:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Routes, Route, useParams} from 'react-router-dom';
+import {Home, About, ProductPage, Contacts, Nopage, Navbar,ProductList} from './pages';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/products/:name" element={<ProductPage/>}/>
+        <Route path="/contacts" element={<Contacts/>}/>
+        <Route path="/product-list" element={<ProductList/>}/>
+        <Route path="*" element={<Nopage/>}/>
+      </Routes>
     </div>
   );
 }
+
 
 export default App;
